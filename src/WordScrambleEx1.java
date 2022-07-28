4d3ffe519d637e518bf7757ae921a0ffb4672407
Youna2022 <108061839+Youna2022@users.noreply.github.com>
2022-07-29 01:00:02 +0900

    Update WordScrambleEx1.java

--- src/WordScrambleEx1.java
+++ src/WordScrambleEx1.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class WordScrambleEx1 {
 
@@ -8,18 +9,43 @@ public class WordScrambleEx1 {
          String answer = getAnswer(strArr);
          String question = getScrambledWord(answer);
 
-         System.out.println("Question:"+question);
-         System.out.println("Answer:"+answer);
-   } // main
+         Scanner sc = new Scanner(System.in);
+         
+         
+         while(true) {
+             System.out.println("Question :" + question);
+             System.out.print("Your answer is :"); 
+             String str = sc.nextLine();
+//             System.out.println(str);
+             
+             if(str.equals("q") || str.equals("Q"))
+            	 break;
+             
+             if(question.equals(str)) {
+            	 System.out.println("정답입니다");
+            	 break;
+             } else {
+            	 System.out.println(str + "은/는 정답이아닙니다. 다시 시도해보세요.");
+             }
 
-   public static String getAnswer(String[] strArr) { 
-	   Random r = new Random();
-	   String answer = strArr[r.nextInt(strArr.length-1)+1];
-	   return answer;
-	   
-//	   int rnd = new Random().nextInt(strArr.length);
-//	   return strArr[rnd];
-   }
+             // 1. 화면을 통해 사용자의 입력을 받는다.(Scanner클래스 사용)
+             // 2. 사용자가 q 또는 Q를 입력하면 프로그램을 종료한다.
+             // 3. 사용자가 정답을 맞출때까지 반복하다가
+             //     사용자가 정답을 맞추면, while문을 빠져나간다.
+             //
+             //Question :HEOP
+//             Your answer is :phoe
+//             phoe은/는 정답이 아닙니다. 다시 시도해보세요.
+//             Question :HEOP
+//             Your answer is :hope
+//             정답입니다.
+       } // while
+
+ } // main
+	  public static String getAnswer(String[] strArr) {
+          int idx = (int)(Math.random()*strArr.length);
+          return strArr[idx];
+    }
    public static String getScrambledWord(String str) {
 	 char[] chArr = str.toCharArray();
 	 
